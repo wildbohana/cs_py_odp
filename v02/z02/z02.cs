ba779ff17eb33bd8f21c078f475d1ac84eabdf0d
wildbohana <83251325+wildbohana@users.noreply.github.com>
2023-03-07 21:49:18 +0100

    Add files via upload

---
+++ v02/z02/z02.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace z02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, List<string>> recnik = new Dictionary<int, List<string>>(10);
+
+            recnik.Add(0, new List<string> { "fifi" });
+            recnik.Add(1, new List<string> { "slobodan", "slobodic" });
+            recnik.Add(2, new List<string> { "filip", "visnjic" });
+            recnik.Add(3, new List<string> { "vuk", "stefanovic", "karadzic" });
+            recnik.Add(4, new List<string> { "filic" });
+            recnik.Add(5, new List<string> { "ilija", "ilic" });
+            recnik.Add(6, new List<string> { "marko", "markovic" });
+            recnik.Add(7, new List<string> { "slobodan", "slobodic" });
+            recnik.Add(8, new List<string> { "stevo", "stevic" });
+            recnik.Add(9, new List<string> { "milica", "milic" });
+
+            for (int i = 0; i < 10; i++)
+            {
+                List<string> str = new List<string>();
+				
+                if (recnik.TryGetValue(i, out str))
+                    for (int j = 0; j < str.Count; j++)
+                        Console.WriteLine(i + " - " + j + " - " + str[j]);
+            }            
+        }
+    }
+}
---
+++ v02/z03/z03.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace z03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> niz = new List<object>(3000000);
+            int ukDuz = 0;
+
+            for (int i = 0; i < niz.Capacity - 1; i++)
+            {
+                if (i % 3 == 0) niz.Add(null);
+                else if (i % 3 == 1) niz.Add("string" + i);
+                else if (i % 3 == 2) niz.Add(new object());
+            }
+
+            for (int i = 0; i < niz.Capacity - 1; i++)
+            {
+                try
+                {
+                    string current = niz[i] as string;
+                    if (current != null)
+                        ukDuz += current.Length;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine("Ukupna duzina stringova: " + ukDuz);
+        }
+    }
+}
