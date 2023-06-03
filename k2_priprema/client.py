6b8d48e33faafa6bb0b532fb3e6b3e2c1b5f179f
wildbohana <83251325+wildbohana@users.noreply.github.com>
2023-06-03 02:03:47 +0200

    штагод

--- k2_priprema/client.py
+++ k2_priprema/client.py
@@ -1,53 +1,51 @@
 import socket 
 
-def main():
-    klijent = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    klijent.connect(('localhost', 6000)) 
-    print("Veza sa serverom je uspostavljena.") 
-    
-    while True:
-        poruka = input("Unesite akciju:") 
-        student:str = ""
-        match poruka:
-            case "add":
-                student = add_student()
-                poruka = poruka + ":" + student
-            case "delete":
-                student = delete_student()
-                poruka = poruka + ":" + student
-            case "edit":
-                student = edit_student()              
-                poruka = poruka + ":" + student
-            case "print":
-                poruka
-                
-        klijent.send(poruka.encode())
-        if not poruka: break
-        
-        poruka = klijent.recv(1024).decode()
-        if not poruka: break
-        print(f"Rezultat je:\n{poruka}")
-        
-    print("Konekcija se zatvara.") 
-    klijent.close()
-
-def add_student():
+# Potrebne funkcije
+def dodaj_studenta():
     ime = input("Unesite ime studenta: ")
     prezime = input("Unesite prezime studenta: ")
     indeks = input("Unesite broj indeksa studenta: ")
     ocene = input("Unesite ocene studenta odvojene razmakom: ").strip()
     return f'{ime}|{prezime}|{indeks}|{ocene}'
 
-def delete_student():
-    indeks= input("Unesite indeks studenta za brisanje: ")
+def obrisi_studenta():
+    indeks = input("Unesite indeks studenta za brisanje: ")
     return indeks
 
-def edit_student():
+def izmeni_studenta():
     indeks = input("Unesite indeks studenta za azuriranje: ")
     ime = input("Unesite novo ime studenta: ")
     prezime = input("Unesite novo prezime studenta: ")
     ocene = input("Unesite nove ocene studenta odvojene razmakom: ").strip()
     return f"{ime}|{prezime}|{indeks}|{ocene}"
 
-if __name__ == "__main__":
-    main()
+# main - sort of lol
+klijent = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+klijent.connect(('localhost', 6000)) 
+print("Veza sa serverom je uspostavljena.") 
+
+while True:
+	poruka = input("Unesite akciju (dodaj / obrisi / izmeni / ispis): ") 
+	student:str = ""
+	match poruka:
+		case "dodaj":
+			student = dodaj_studenta()
+			poruka = poruka + ":" + student
+		case "obrisi":
+			student = obrisi_studenta()
+			poruka = poruka + ":" + student
+		case "izmeni":
+			student = izmeni_studenta()              
+			poruka = poruka + ":" + student
+		case "ispis":
+			poruka
+			
+	klijent.send(poruka.encode())
+	if not poruka: break
+	
+	poruka = klijent.recv(1024).decode()
+	if not poruka: break
+	print("Rezultat je:\n" + poruka)
+	
+print("Veza se zatvara.") 
+klijent.close()
--- k2_priprema/server.py
+++ k2_priprema/server.py
@@ -1,67 +1,68 @@
 import socket
 from student import Student
 
-studenti = {}
-
-def server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('localhost', 6000))
-    server.listen()
-    print("Server je pokrenut i ceka klijente.")
-    
-    klijent, adresa = server.accept()
-    print(f"Klijent {adresa} se povezao na server.")
-    
-    while True:
-        poruka = klijent.recv(1024).decode()
-        if not poruka: break
-        print(f"Primljena poruka: {poruka}")
-        
-        parts = poruka.strip().split(":")
-        action = parts[0]
-        student = parts[1] if len(parts) > 1 else ""
-        
-        if action == "add":
-            student = student.split("|")
-            student = Student(student[0], student[1], student[2], student[3])
-            poruka = add_student(student)
-        elif action == "delete":
-            poruka = delete_student(student)
-        elif action == "edit":
-            student = student.split("|")
-            student = Student(student[0], student[1], student[2], student[3])
-            poruka = edit_student(student)
-        elif action == "print":
-            poruka = print_students()
-            
-        klijent.send(poruka.encode())
-        
-    print(f"Klijent {adresa} se diskonektovao.")
-    klijent.close()
-
-def add_student(student:Student) -> str:
+# Potrebne funkcije
+def dodaj_studenta(student:Student) -> str:
     if student.broj_indeksa in studenti:
         return "Student sa tim brojem indeksa vec postoji."
     studenti[student.broj_indeksa] = student
     return "Student je uspesno dodat."
 
-def delete_student(indeks:str) -> str:
+def obrisi_studenta(indeks:str) -> str:
     if indeks not in studenti:
         return "Student sa tim brojem indeksa ne postoji."
     del studenti[indeks]
     return "Student je uspesno obrisan."
 
-def edit_student(student:Student) -> str:
+def izmeni_studenta(student:Student) -> str:
     if student.broj_indeksa not in studenti:
         return "Student sa tim brojem indeksa ne postoji."
     studenti[student.broj_indeksa] = student
     return "Student je uspesno azuriran."
 
-def print_students() -> str:
-    if len(studenti) == 0:
-        return "Nema studenata."
-    return "\n".join(["\n=======================\n"+str(studenti[indeks]) for indeks in studenti])
+def ispisi_studente() -> str:
+	if len(studenti) == 0:
+		return "Nema studenata."
+	ispis = ""
+	for indeks in studenti:
+		ispis += str(studenti[indeks])
+	return ispis
 
-if __name__ == "__main__":
-    server()
-    studenti.clear()
+# main - sort of
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('localhost', 6000))
+server.listen()
+print("Server je pokrenut i ceka klijente.")
+
+# Rečnik
+studenti = {}
+
+kanal, adresa = server.accept()
+print(f"Klijent {adresa} se povezao na server.")
+
+while True:
+	poruka = kanal.recv(1024).decode()
+	if not poruka: break
+	print(f"Primljena poruka: {poruka}")
+	
+	delovi = poruka.strip().split(":")
+	akcija = delovi[0]
+	student = delovi[1] if len(delovi) > 1 else ""
+	
+	if akcija == "dodaj":
+		student = student.split("|")
+		student = Student(student[0], student[1], student[2], student[3])
+		poruka = dodaj_studenta(student)
+	elif akcija == "obrisi":
+		poruka = obrisi_studenta(student)
+	elif akcija == "izmeni":
+		student = student.split("|")
+		student = Student(student[0], student[1], student[2], student[3])
+		poruka = izmeni_studenta(student)
+	elif akcija == "ispis":
+		poruka = ispisi_studente()
+		
+	kanal.send(poruka.encode())
+	
+print(f"Klijent {adresa} se odvezao.")
+kanal.close()	# kanal ili server ???
